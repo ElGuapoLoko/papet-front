25bcb4107efdab9dd14e6664c31dc826a9905075
Henrique Siqueira <h.enrique.siq@hotmail.com>
2022-11-27 08:42:10 -0300

    Wip

--- src/app/components/hackathon/register/register.component.ts
+++ src/app/components/hackathon/register/register.component.ts
@@ -1,23 +1,29 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {RegisterService} from "./services/register.service";
+import {Router} from "@angular/router";
+import {MessageService} from "primeng/api";
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss'],
     encapsulation: ViewEncapsulation.None,
+    providers: [MessageService]
 })
 export class RegisterComponent implements OnInit {
 
     optionsTypes: any[] = [
-        {name: 'Veterinário', id: 1},
-        {name: 'Dono de pet', id: 2},
+        {id: 1, name: 'Veterinário', value: 'MEDIC'},
+        {id: 2, name: 'Dono de pet', value: 'OWNER'},
     ];
-
+    name: string = '';
+    cpf: string = '';
+    telefone: string = '';
+    email: string = '';
+    password: string = '';
     selectedType: any = null;
 
-
-    constructor() {
-
+    constructor(private registerService: RegisterService, private router: Router, private messageService: MessageService) {
         this.selectedType = this.optionsTypes[0]
     }
 
@@ -25,9 +31,24 @@ export class RegisterComponent implements OnInit {
     }
 
     register(): void {
+        const payload = {
+            name: this.name,
+            cpf: this.cpf,
+            telefone: this.telefone,
+            email: this.email,
+            password: this.password,
+            selectedType: this.selectedType.value
+        };
 
+        this.registerService.register(payload).subscribe((response) => {
+            if (response.status == 'success') {
+                this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Usuário criado'});
+                this.router.navigate(['/login']);
+            }
+            if (response.status == 'error') {
+                this.messageService.add({severity: 'error', summary: 'Erro', detail: response.message});
+            }
+        });
 
     }
-
-
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,8 @@ import {ConfigService} from '../../service/app.config.service';
 import {AppConfig} from '../../api/appconfig';
 import {Subscription} from 'rxjs';
 import {Router} from "@angular/router";
+import {LoginService} from "./services/login.service";
+import {MessageService} from "primeng/api";
 
 @Component({
     selector: 'app-login',
@@ -12,14 +14,15 @@ import {Router} from "@angular/router";
 export class LoginComponent implements OnInit, OnDestroy {
 
     valCheck: string[] = ['remember'];
-
     password: string;
-
+    email: string;
     config: AppConfig;
-
     subscription: Subscription;
 
-    constructor(public configService: ConfigService, private router: Router) {
+    constructor(public configService: ConfigService,
+                private loginService: LoginService,
+                private messageService: MessageService,
+                private router: Router) {
     }
 
     ngOnInit(): void {
@@ -36,7 +39,19 @@ export class LoginComponent implements OnInit, OnDestroy {
     }
 
     login(): void {
-        this.router.navigate(['/dashboard'])
+            const payload = {
+                email: this.email,
+                password: this.password,
+            };
+
+            this.loginService.login(payload).subscribe((response) => {
+                if (response.status == 'success') {
+                    this.router.navigate(['/login']);
+                }
+                if (response.status == 'error') {
+                    this.messageService.add({severity: 'error', summary: 'Erro', detail: response.message});
+                }
+            });
     }
 
 }
